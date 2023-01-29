ac5d4571c84dacc07b5cd0155492c696264e32d3
Kazuki Tsunemi <ksilverwall@gmail.com>
2023-01-29 22:30:32 +0900

    accept request from localhost

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,44 @@
 extern crate daemonize;
 
-use std::fs::File;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Error},
+    net::TcpListener,
+    thread,
+    time::Duration,
+};
 
 use daemonize::Daemonize;
+
+fn accept_requests() -> Result<(), Error> {
+    let listener = TcpListener::bind("127.0.0.1:10080")?;
+    let jh = thread::spawn(move || loop {
+        match listener.accept() {
+            Ok((socket, addr)) => {
+                println!("new client: {addr:?}");
+                let mut reader = BufReader::new(socket);
+                thread::spawn(move || loop {
+                    let mut rcv_data = String::new();
+                    match reader.read_line(&mut rcv_data) {
+                        Ok(_) => {
+                            if rcv_data.len() == 0 {
+                                thread::sleep(Duration::from_millis(10))
+                            } else {
+                                println!("handled message: {rcv_data:?}");
+                            }
+                        }
+                        Err(e) => println!("no message: {e:?}"),
+                    }
+                });
+            }
+            Err(e) => println!("couldn't get client: {e:?}"),
+        }
+    });
+    if let Err(_) = jh.join() {
+        println!("failed to join thread");
+    }
+    Ok(())
+}
 
 fn main() {
     let stdout = File::create("/tmp/daemon.out").unwrap();
@@ -17,6 +53,11 @@ fn main() {
 
     match daemonize.start() {
         Ok(_) => println!("Success, daemonized"),
+        Err(e) => eprintln!("Error, {}", e),
+    }
+
+    match accept_requests() {
+        Ok(_) => println!("Terminated"),
         Err(e) => eprintln!("Error, {}", e),
     }
 }
