8d9651d04658a4ab53cf33027089dbc1582e1dd7
Kazuki Tsunemi <ksilverwall@gmail.com>
2023-01-29 19:50:13 +0900

    deamonize process

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main () {
-    println!("Hello, world!");
+extern crate daemonize;
+
+use std::fs::File;
+
+use daemonize::Daemonize;
+
+fn main() {
+    let stdout = File::create("/tmp/daemon.out").unwrap();
+    let stderr = File::create("/tmp/daemon.err").unwrap();
+
+    let daemonize = Daemonize::new()
+        .pid_file("/tmp/test.pid")
+        .working_directory("/tmp")
+        .stdout(stdout)
+        .stderr(stderr)
+        .privileged_action(|| "Executed before drop privileges");
+
+    match daemonize.start() {
+        Ok(_) => println!("Success, daemonized"),
+        Err(e) => eprintln!("Error, {}", e),
+    }
 }
