d297a09f6da0adfaf133941f5e1761420482a097
Kazuki Tsunemi <ksilverwall@gmail.com>
2023-02-06 14:37:34 +0900

    Implement 2 request message types

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -1,36 +1,64 @@
 use std::{
     io::{BufRead, BufReader, Error, Write},
-    net::{TcpStream, ToSocketAddrs},
+    net::TcpStream,
 };
 
 const HOST: &str = "server";
 const PORT: i32 = 10080;
 
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct GetMessageRequest {
+    request_type: String,
+}
+
+#[derive(Serialize)]
+struct SendMessageRequest {
+    request_type: String,
+    message: String,
+}
+
 fn main() -> Result<(), Error> {
-    let socket_addrs = format!("{HOST}:{PORT}")
-        .to_socket_addrs()
-        .expect("Failed to get socket addr from hostname");
+    let mut sock = TcpStream::connect(format!("{HOST}:{PORT}")).expect("Failed to connect");
 
-    for addr in socket_addrs {
-        let mut sock = TcpStream::connect(addr).expect("Failed to connect");
-        match sock.write_all("Hello, TCP\r\n".as_bytes()) {
+    {
+        let m_req = GetMessageRequest {
+            request_type: "GET_MESSAGE".to_string(),
+        };
+
+        let content = serde_json::to_string(&m_req).unwrap();
+        match sock.write_all((content + "\r\n").as_bytes()) {
+            Ok(()) => println!("send GET MESSAGE request"),
+            Err(v) => println!("send test message failed:{}", v),
+        }
+    }
+
+    //
+    // Send Messages
+    //
+    let messages = ["Hello Tcp".to_string(), "World End".to_string()];
+    for m in messages {
+        let m_req = SendMessageRequest {
+            request_type: "SEND_MESSAGE".to_string(),
+            message: m,
+        };
+
+        let content = serde_json::to_string(&m_req).unwrap();
+        match sock.write_all((content + "\r\n").as_bytes()) {
             Ok(()) => println!("send test message success"),
             Err(v) => println!("send test message failed:{}", v),
         }
-        match sock.write_all("World End\r\n".as_bytes()) {
-            Ok(()) => println!("send test message success"),
-            Err(v) => println!("send test message failed:{}", v),
-        }
+    }
 
-        let mut rcv_data = String::new();
-        let mut reader = BufReader::new(sock.try_clone().unwrap());
-        reader.read_line(&mut rcv_data).unwrap();
-        print!("{rcv_data}");
-
+    //
+    // Receive Messages
+    //
+    let mut reader = BufReader::new(sock.try_clone().unwrap());
+    for _ in 0..3 {
         let mut rcv_data = String::new();
         reader.read_line(&mut rcv_data).unwrap();
         print!("{rcv_data}");
-        break;
     }
 
     Ok(())
